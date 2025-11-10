f1a54e8dab03c871272141b7a02d7cded763105e
volan4ik39 <vevelvova2009@gmail.com>
2025-11-10 16:47:16 +0000

    1

--- less_3.2.py
+++ less_3.2.py
@@ -1,10 +1,9 @@
 import const
 import numpy
-fact=numpy.math.factorial
 h = 100
 α = 45
 β = 35
-from numpy.math import*
+from numpy import*
 from const import *
 v = ((g*h)* (math.tan(β)**2))/(2* (math.cos(α)**2)*(1-math.tan(β)*math.tan(α)))**0.5
 print(v)
