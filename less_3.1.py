78884699f6476d2116d85c01716f9ae7cc381f41
volan4ik39 <vevelvova2009@gmail.com>
2025-11-10 16:38:57 +0000

    лаба3

---
+++ less_3.1.py
@@ -0,0 +1,12 @@
+import const
+from const import *
+print(a)
+print(b)
+print(c)
+print(d)
+print(l)
+print(f)
+print(g)
+print(h)
+print(i)
+print(j)
---
+++ less_3.2.py
@@ -0,0 +1,10 @@
+import const
+import numpy
+fact=numpy.math.factorial
+h = 100
+α = 45
+β = 35
+from numpy.math import*
+from const import *
+v = ((g*h)* (math.tan(β)**2))/(2* (math.cos(α)**2)*(1-math.tan(β)*math.tan(α)))**0.5
+print(v)
